585de8cf06987b97259b65b967b391568fc5162e
BartOomsschool <r0445162@student.thomasmore.be>
2019-11-08 09:06:52 +0100

    Eureka-server

---
+++ AthleterBackEnd/Eureka/src/test/java/com/team9/Eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team9.Eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
