24befc606d9d95c5a97a8bc0944753835ad0820e
BartOomsschool <r0445162@student.thomasmore.be>
2019-11-21 14:04:04 +0100

    service inorde brengen

--- AthleterFrontEnd/src/app/services/voetbal/positie/positie.service.ts
+++ AthleterFrontEnd/src/app/services/voetbal/positie/positie.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Positie } from 'src/app/models/voetbal/positie/positie.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PositieService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getPosities(): Observable<Positie[]> {
+      return this.http.get<Positie[]>("https://localhost:8050/posities/getPosities");
+  }
+
+  addPositie(positie: Positie) {
+      return this.http.post<Positie>("https://localhost:8050/posities/postTeam", positie);
+  }
+
+  updatePositie(positieID: number, positie: Positie) {
+      return this.http.put<Positie>("https://localhost:8050/posities/updatePositie/" + positieID, positie);
+  }
+
+  deletePositie(positieID: number) {
+      return this.http.delete<Positie>("https://localhost:8050/posities/deletePositie/" + positieID);
+  }
 }
--- AthleterFrontEnd/src/app/services/voetbal/team/team.service.ts
+++ AthleterFrontEnd/src/app/services/voetbal/team/team.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Team } from 'src/app/models/voetbal/team/team.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TeamService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getTeams(): Observable<Team[]> {
+      return this.http.get<Team[]>("https://localhost:8050/teams/getTeams");
+  }
+
+  addTeam(team: Team) {
+      return this.http.post<Team>("https://localhost:8050/teams/postTeam", team);
+  }
+
+  updateTeam(teamID: number, team: Team) {
+      return this.http.put<Team>("https://localhost:8050/teams/updateTeam/" + teamID, team);
+  }
+
+  deleteTeam(teamID: number) {
+      return this.http.delete<Team>("https://localhost:8050/teams/deleteTeam/" + teamID);
+  }
 }
