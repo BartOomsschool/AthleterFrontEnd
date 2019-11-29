d40b0673fe1f908ae0dcf4c8b919cd07e454ecf1
Jasey <Jwaegebaert@gmail.com>
2019-11-29 10:10:21 +0100

    nav + footer

--- AthleterFrontEnd/src/app/app.module.ts
+++ AthleterFrontEnd/src/app/app.module.ts
@@ -7,31 +7,25 @@ import { HttpClientModule } from '@angular/common/http';
 import { VoetbalComponent } from './views/voetbal/voetbal/voetbal.component';
 import { SpelerToevoegenComponent } from './views/voetbal/speler-toevoegen/speler-toevoegen/speler-toevoegen.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-<<<<<<< HEAD
-import { BadmintonComponent } from './views/badminton/badminton/badminton.component';
-=======
 import { VoetbalTeamToevoegenComponent } from './views/voetbal/voetbal-team-toevoegen/voetbal-team-toevoegen/voetbal-team-toevoegen.component';
 import { VoetbalPositieToevoegenComponent } from './views/voetbal/voetbal-positie-toevoegen/voetbal-positie-toevoegen/voetbal-positie-toevoegen.component';
->>>>>>> e378ba885fd2cd7251ce4d86c8ca2d6060461a22
+import { BadmintonModule } from './views/badminton/badminton.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     VoetbalComponent,
     SpelerToevoegenComponent,
-<<<<<<< HEAD
-    BadmintonComponent
-=======
     VoetbalTeamToevoegenComponent,
     VoetbalPositieToevoegenComponent
->>>>>>> e378ba885fd2cd7251ce4d86c8ca2d6060461a22
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     SharedModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    BadmintonModule
 
   ],
   providers: [],
