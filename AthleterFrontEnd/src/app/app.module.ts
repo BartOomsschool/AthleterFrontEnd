65ede8cecc6293f51f3e2122a6ac816047297294
BartOomsschool <r0445162@student.thomasmore.be>
2019-11-29 11:50:29 +0100

    fix merge conflict

--- AthleterFrontEnd/src/app/app.module.ts
+++ AthleterFrontEnd/src/app/app.module.ts
@@ -7,27 +7,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { VoetbalComponent } from './views/voetbal/voetbal/voetbal.component';
 import { VoetbalAtleetToevoegenComponent } from './views/voetbal/voetbalatleet-toevoegen/voetbalatleet-toevoegen/voetbalatleet-toevoegen.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-<<<<<<< HEAD
-import { BadmintonComponent } from './views/badminton/badminton/badminton.component';
-
-=======
-import { VoetbalTeamToevoegenComponent } from './views/voetbal/voetbal-team-toevoegen/voetbal-team-toevoegen/voetbal-team-toevoegen.component';
-import { VoetbalPositieToevoegenComponent } from './views/voetbal/voetbal-positie-toevoegen/voetbal-positie-toevoegen/voetbal-positie-toevoegen.component';
 import { BadmintonModule } from './views/badminton/badminton.module';
->>>>>>> 859e5e415ebf31eb18b26d0c776bc61fee204d86
 
 @NgModule({
   declarations: [
     AppComponent,
     VoetbalComponent,
-<<<<<<< HEAD
     VoetbalAtleetToevoegenComponent,
-    BadmintonComponent,
-=======
-    SpelerToevoegenComponent,
-    VoetbalTeamToevoegenComponent,
-    VoetbalPositieToevoegenComponent
->>>>>>> 859e5e415ebf31eb18b26d0c776bc61fee204d86
   ],
   imports: [
     BrowserModule,
